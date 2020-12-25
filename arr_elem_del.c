c67c69a84bac5259719a6e6741b363c1977dc58d
thisisfurky <64855359+thisisfurky@users.noreply.github.com>
2020-12-25 19:46:36 +0530

    Day 2 Assignment Submission

---
+++ arr_elem_del.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[100],pos,n,i;
+    printf("Enter the no of elements of array\n");
+    scanf("%d",&n);
+    printf("Enter %d no of elements\n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the position of element you wish to delete\n");
+    scanf("%d",&pos);
+    if (pos>=n){
+        printf("Deletion not possible\n");
+    }
+    else{
+        for(i=pos;i<n-1;i++){
+            arr[i] = arr[i+1];
+        }
+    }
+    for(i=0;i<n-1;i++){
+        printf("%d\n",arr[i]);
+    }
+}
