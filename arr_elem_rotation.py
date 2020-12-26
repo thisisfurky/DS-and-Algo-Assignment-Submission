21819adef911003c633bb549d18438ee1700d2d5
thisisfurky <64855359+thisisfurky@users.noreply.github.com>
2020-12-26 15:23:40 +0530

    Day 2 Assignment

---
+++ arr_elem_rotation.py
@@ -0,0 +1,22 @@
+def arrayRotate(arr,n,k):
+    for i in range(k):
+        temp = arr[0]
+        for x in range(n-1):
+            arr[x] = arr[x+1]
+        arr[n-1] = temp
+
+
+def printArray(arr,n):
+    for i in range(0,n):
+        print(arr[i],end=' ')
+
+
+n = int(input("Enter the no of elements of array"))
+print("Enter the elements")
+arr = []
+for i in range(0,n):
+    element = int(input())
+    arr.append(element)
+k = int(input("Enter the no of times you want to rotate the array"))
+arrayRotate(arr,n,k)
+printArray(arr,n)
